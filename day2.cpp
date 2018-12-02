a85c0db43119ed2d9c57ac0ce73e574a11325bc7
Vincent Lee <vincent.lee@outlook.com>
2018-12-01 23:30:01 -0600

    Day 2

--- day2.cpp
+++ day2.cpp
@@ -1,7 +1,57 @@
 #include "util.h"
+#include <regex>
 
 namespace day2 {
-void run() {
 
+static std::tuple<bool, bool> count_double_triple(const std::string& s) {
+    std::unordered_map<char, long> map;
+    for (char c : s) {
+        map[c]++;
+    }
+    bool has_double = false;
+    bool has_triple = false;
+    for (const auto& pair : map) {
+        if (pair.second == 2) {
+            has_double = true;
+        } else if (pair.second == 3) {
+            has_triple = true;
+        }
+    }
+    return std::make_tuple(has_double, has_triple);
+}
+
+static bool has_one_diff(const std::string& s1, const std::string& s2) {
+    long diffs = 0;
+    for (unsigned int i = 0; i < s1.length(); i++) {
+        if (s1[i] != s2[i])
+            diffs++;
+    }
+    return diffs == 1;
+}
+
+void run() {
+    std::ifstream f("d2_input.txt");
+    std::vector<std::string> lines;
+    std::copy(std::istream_iterator<std::string>(f), {}, std::back_inserter(lines));
+    long count_double = 0, count_triple = 0;
+    for (const auto& s : lines) {
+        auto [has_double, has_triple] = count_double_triple(s);
+        if (has_double)
+                count_double++;
+        if (has_triple)
+                count_triple++;
+    }
+    std::cout << "p1: " << count_double * count_triple << std::endl;
+
+    for (unsigned int i = 0; i < lines.size(); i++) {
+        for (unsigned int j = i + 1; j < lines.size(); j++) {
+            if (has_one_diff(lines[i], lines[j])) {
+                std::cout << "p2:" << std::endl;
+                std::cout << lines[i] << std::endl;
+                std::cout << lines[j] << std::endl;
+                return;
+            }
+        }
+    }
 }
 }
