4b7673909990dc8ef35ee26394902ef03c67b669
Vincent Lee <vincent.lee@outlook.com>
2018-12-10 23:59:24 -0600

    d11-2

--- src/day11.cpp
+++ src/day11.cpp
@@ -14,6 +14,40 @@ static int64_t power(int64_t x, int64_t y) {
 	return hundreds - 5;
 }
 
+static void run_p2(const std::array<std::array<int64_t, 300>, 300>& grid) {
+	int64_t max_score = std::numeric_limits<int64_t>::min();
+	uint32_t max_row, max_col, size;
+
+	for (uint32_t row = 1; row <= 300; row++) {
+		for (uint32_t col = 1; col <= 300; col++) {
+			int64_t local_score = 0;
+			// try expanding rightward and downward
+			for (uint32_t expansion = 0; row+expansion <= 300 && col+expansion <= 300; expansion++) {
+				// right edge excluding the corner
+				for (uint32_t new_row = row; new_row <= row + expansion - 1; new_row++) {
+					uint32_t new_col = col + expansion;
+					local_score += grid.at(new_col-1).at(new_row-1);
+				}
+
+				// bottom edge including the corner
+				for (uint32_t new_col = col; new_col <= col + expansion; new_col++) {
+					uint32_t new_row = row + expansion;
+					local_score += grid.at(new_col-1).at(new_row-1);
+				}
+
+				if (local_score > max_score) {
+					max_score = local_score;
+					max_row = row;
+					max_col = col;
+					size = expansion + 1;
+				}
+			}
+		}
+	}
+
+	std::cout << "p2: " << max_col << "," << max_row << "," << size << std::endl;
+}
+
 void run() {
 	// ~720K, should be fine on linux...
 	std::array<std::array<int64_t, 300>, 300> grid;
@@ -59,5 +93,6 @@ void run() {
 		}
 	}
 	std::cout << "p1: " << max_col << ", " << max_row << std::endl;
+	run_p2(grid);
 }
 }
