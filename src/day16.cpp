4ab8e6d952aef5885c9f80170a4460760317bdab
Vincent Lee <vincent.lee@outlook.com>
2018-12-16 11:54:04 -0600

    Clean up d16

--- src/day16.cpp
+++ src/day16.cpp
@@ -92,54 +92,15 @@ struct sample {
 	vec<opcode> candidates;
 };
 
-static vec<opcode> test(const sample& s) {
+static vec<opcode> get_candidates(const sample& s) {
 	vec<opcode> ret;
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] + s.old_state[s.insn.in2]))
-		ret.push_back(ADDR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] + s.insn.in2))
-		ret.push_back(ADDI);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] * s.old_state[s.insn.in2]))
-		ret.push_back(MULR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] * s.insn.in2))
-		ret.push_back(MULI);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] & s.old_state[s.insn.in2]))
-		ret.push_back(BANR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] & s.insn.in2))
-		ret.push_back(BANI);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] | s.old_state[s.insn.in2]))
-		ret.push_back(BORR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] | s.insn.in2))
-		ret.push_back(BORI);
-	if (s.new_state[s.insn.out] == s.old_state[s.insn.in1])
-		ret.push_back(SETR);
-	if (s.new_state[s.insn.out] == s.insn.in1)
-		ret.push_back(SETI);
-	if (s.new_state[s.insn.out] == (s.insn.in1 > s.old_state[s.insn.in2]))
-		ret.push_back(GTIR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] > s.insn.in2))
-		ret.push_back(GTRI);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] > s.old_state[s.insn.in2]))
-		ret.push_back(GTRR);
-	if (s.new_state[s.insn.out] == (s.insn.in1 == s.old_state[s.insn.in2]))
-		ret.push_back(EQIR);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] == s.insn.in2))
-		ret.push_back(EQRI);
-	if (s.new_state[s.insn.out] == (s.old_state[s.insn.in1] == s.old_state[s.insn.in2]))
-		ret.push_back(EQRR);
-	return ret;
-}
-
-static void show(const sample& s) {
-	std::cout << "Before: " << s.old_state[0] << "," << s.old_state[1] << "," << s.old_state[2] << "," << s.old_state[3] << std::endl;
-	std::cout << "insn: " << s.insn.op << "," << s.insn.in1 << "," << s.insn.in2 << "," << s.insn.out << std::endl;
-	std::cout << "After: " << s.new_state[0] << "," << s.new_state[1] << "," << s.new_state[2] << "," << s.new_state[3] << std::endl;
-}
-
-static void show_insns(const vec<opcode>& ops) {
-	for (const auto op : ops) {
-		std::cout << op << ' ';
+	for (opcode o : {ADDR, ADDI, MULR, MULI, BANR, BANI, BORR, BORI, SETR, SETI, GTIR, GTRI, GTRR, EQIR, EQRI, EQRR}) {
+		auto candidate = s.insn;
+		candidate.op = o;
+		if (execute(candidate, s.old_state) == s.new_state)
+			ret.push_back(o);
 	}
-	std::cout << std::endl;
+	return ret;
 }
 
 void run() {
@@ -160,7 +121,7 @@ void run() {
 				throw std::runtime_error("Trying to read after state with no sample in progress");
 
 			in_progress_sample->new_state = parse_state(line.substr(7));	
-			in_progress_sample->candidates = test(*in_progress_sample);
+			in_progress_sample->candidates = get_candidates(*in_progress_sample);
 			
 			samples.push_back(std::move(*in_progress_sample));
 			in_progress_sample = {};
@@ -203,7 +164,6 @@ void run() {
 
 		// If one candidate opcode remains, we have a new mapping
 		if (next.candidates.size() == 1) {
-			std::cout << "Gained new mapping" << std::endl;
 			mapping[next.insn.op] = next.candidates[0];
 		}
 
